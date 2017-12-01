c36f9124b86bbec0e2910a6034df48313926b2f7
Erick Almeida <ephillipe@gmail.com>
2017-12-01 13:30:40 -0200

    Updating template to support Openshift Origin Deployment

--- scripts/origin/register.sh
+++ scripts/origin/register.sh
@@ -6,6 +6,29 @@ REDHAT_POOLID=$3
 yum clean all
 rm -rf /var/cache/yum
 
-subscription-manager register --username=${REDHAT_USERNAME} --password=${REDHAT_PASSWORD} --force || subscription-manager clean && subscription-manager register --username=${REDHAT_USERNAME} --password=${REDHAT_PASSWORD} --force 
-subscription-manager attach --pool=${REDHAT_POOLID}
-subscription-manager repos --enable="rhel-7-server-ose-3.6-rpms" --enable="rhel-7-server-extras-rpms" --enable="rhel-7-fast-datapath-rpms"
+function log {
+    if ! [ -f /usr/bin/cowsay ]; then
+        sudo yum install -y cowsay
+    fi    
+    cowsay -f tux "$1"
+    sleep 3
+}
+
+function install_prereqs {    
+    if ! [ -f /usr/bin/ansible ]; then
+        log "Instalando pre-requisitos para rodar a instalação do Openshift"
+        sudo yum groupinstall "Development Tools" -y
+        sudo yum install -y wget git net-tools bind-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct    
+        sudo yum install -y python-pip python-devel htop python-passlib python-cryptography pyOpenSSL httpd-tools     
+        
+        log "Instalando pre-requisitos para rodar o sistema de métricas"
+        sudo yum install -y java-1.8.0-openjdk-headless
+        
+        log "Instalando Ansible"
+        sudo pip install --upgrade pip
+        sudo pip install --upgrade ansible apache-libcloud
+    fi
+    install_networkmanager_on_all
+}
+
+install_prereqs
